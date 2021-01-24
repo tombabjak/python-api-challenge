c349ce638b6455bdca09a75a7371b6027bb3e23b
Tom Babjak <thomas.babjak@gmail.com>
2021-01-23 21:27:14 -0500

    third commit

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "58fbbaef556ef8bb9ad02f4fc02da1b5"
+weather_api_key = "0f8d3ff5c8b0eefad692682d7e393809"
 
 # Google API Key
 g_key = "AIzaSyC459iumVXEP5g1t-q8M3YyGah_ZfW-CNc"
